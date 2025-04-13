f4e4858d26dd08b45cf479194d3076101d6ba9ac
YangW796 <wy22302010061@163.com>
2025-04-13 23:20:41 +0800

    task1:v1

--- demo_test.py
+++
@@ -1,9 +0,0 @@
-import pybullet as p
-import math
-
-# 欧拉角，单位是弧度
-euler_angles = [math.radians(30), math.radians(45), math.radians(60)]
-
-# 获取四元数
-quaternion = p.getQuaternionFromEuler(euler_angles)
-print(quaternion)
